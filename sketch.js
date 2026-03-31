55800371a77b4abd157107080129b038ad70fda6
gciocc <g.ciocca@ied.edu>
2026-03-31 12:32:35 +0200

    omini

--- sketch.js
+++ sketch.js
@@ -1,14 +1,21 @@
-let PePERLIN_SCALE = 200;
-let TITLE_SIZE = 32;
+let PERLIN_SCALE = 200;
+let TITLE_SIZE = 10;
+let SPRITE_SIZE = 32;
 
 let grassImage;
 let sandImage;
 let waterImage;
 
+let ominaImage;
+let ominoImage;
+
 function preload() {
-  grassImage = loadImage("tiles/prato.png");
-  sandImage = loadImage("tiles/sabbia.png");
-  waterImage = loadImage("tiles/acqua.png");
+  grassImage = loadImage("assets/tiles/prato.png");
+  sandImage = loadImage("assets/tiles/sabbia.png");
+  waterImage = loadImage("assets/tiles/acqua.png");
+
+  ominaImage = loadImage("assets/sprites/omina.png");
+  ominoImage = loadImage("assets/sprites/omino.png");
 }
 
 function setup() {
@@ -34,7 +41,7 @@ function setup() {
 
       // Perlin
       noiseDetail(6); // Dettaglio del rumore
-      let perlin = noise(x / PePERLIN_SCALE, y / PePERLIN_SCALE); // Scala per variare la frequenza
+      let perlin = noise(x / PERLIN_SCALE, y / PERLIN_SCALE); // Scala per variare la frequenza
       altitude *= perlin;
       altitude += perlin;
       altitude -= 0.5;;
@@ -43,7 +50,8 @@ function setup() {
       let sealevel = 0.2; // Soglia per il livello del mare
       let beachLevel = 0.28; // Soglia per la spiaggia
 
-      let img;
+       // Tiles
+       let img;
        if (altitude < sealevel) {
         img = waterImage; // Acqua
       } else if (altitude < beachLevel) {
@@ -51,9 +59,42 @@ function setup() {
       } else {
         img = grassImage; // Terra
       }
-      
-
       image(img, x, y, TITLE_SIZE, TITLE_SIZE);
+     }
+  }
+
+  for (let x = 0; x < width; x = x + SPRITE_SIZE) {
+    for (let y = 0; y < height; y = y + SPRITE_SIZE) {
+      fill(x, y, 0);
+      rect(x, y, 1, 1);
+      console.log(x, y);
+
+      // Calcolo distanza dal centro
+      let distanceFromCenter = dist(centerX, centerY, x, y);
+      let normDistanceFromCenter = distanceFromCenter / (width / 2); // 0-1
+      
+      // Calcolo altitudine
+      let altitude = 1 - normDistanceFromCenter; // 1 al centro, 0 ai bordi
+
+      // Perlin
+      noiseDetail(6); // Dettaglio del rumore
+      let perlin = noise(x / PERLIN_SCALE, y / PERLIN_SCALE); // Scala per variare la frequenza
+      altitude *= perlin;
+      altitude += perlin;
+      altitude -= 0.5;;
+
+      // Assegniamo il colore
+      let sealevel = 0.2; // Soglia per il livello del mare
+      let beachLevel = 0.28; // Soglia per la spiaggia
+      
+      // Omina
+       if (random() < 0.1 && altitude > beachLevel) {
+        image (ominaImage, x, y, SPRITE_SIZE, SPRITE_SIZE);
+        }
+       // Omino
+       if (random() < 0.1 && altitude > beachLevel) {
+        image (ominoImage, x, y, SPRITE_SIZE, SPRITE_SIZE); 
+       }
      }
   }
 }
